7b7c41f1cc932526af65104eea58be25aa16252c
Alex Simon Pitarque <alex.simon@sage.com>
2022-10-16 23:46:57 +0200

    Delete folders

--- Pattern.Repository/MSQLConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pattern.Repository
-{
-    public class MSQLConfiguration
-    {
-        public string connectionString { get; set; }
-
-        public MSQLConfiguration(string connectionString) => this.connectionString = connectionString;
-    }
-}
--- Repository.Pattern/Domain/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Repository.Pattern.Domain
-{
-    public class Product
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public double Price { get; set; }
-    }
-}
